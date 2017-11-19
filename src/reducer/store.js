9ce1dc098fd90acea3a0c545edb0ccd4f8e07627
ravirohit <ravi.swd.rohit@gmail.com>
2017-11-19 12:40:50 +0530

    Store note update

--- src/reducer/store.js
+++ src/reducer/store.js
@@ -1,6 +1,8 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {userReducer} from './userreducer';
-import {expense} from './expensereducer'
 
-export const store = createStore(combineReducers({userReducer,expense}));
-store.subscribe(()=>console.log("subscribe called"));   
+export const store = createStore(combineReducers({userReducer}));
+store.subscribe(() => {
+    // it does get call after every dispatch operation and print the current state about each each reducer
+    console.log("current state of the store:",store.getState());
+});   
